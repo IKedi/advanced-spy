14c178dbb2ba81bd13f5622a2656a788980f2d09
IKedi <36607876+IKedi@users.noreply.github.com>
2022-04-12 20:24:16 +0000

    Update messagesystem.lua

--- messagesystem.lua
+++ messagesystem.lua
@@ -4,7 +4,7 @@ module.OnChat = function(plr, msg, cmo, gui) --No WIP whisper detection for you 
     repeat wait() until cmo
     local a = string.sub(msg, 1, 1):match('%p') and string.sub(msg, 2, 2):match('%a') and string.len(msg) >= 5
 
-	if a and Gui.RoleplayEmphasizer:GetAttribute("Checked") == true then
+	if a and gui.RoleplayEmphasizer:GetAttribute("Checked") == true then
 		cmo(plr, msg, Color3.new(255, 0, 0))
 	else
 		cmo(plr, msg)
